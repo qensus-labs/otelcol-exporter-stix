8b7fbb5aa212bd2f68b64f20d910a9b4937fcbcb
avwsolutions <arnold.van.wijnbergen@gmail.com>
2026-07-19 17:46:46 +0200

    Implement initial STIX Collector exporter skeleton

--- exporter/stixexporter/exporter.go
+++ exporter/stixexporter/exporter.go
@@ -1,5 +1,45 @@
 package stixexporter
 
-// Exporter represents the STIX exporter.
-type Exporter struct {
+import (
+	"context"
+
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/exporter"
+	"go.opentelemetry.io/collector/exporter/exporterhelper"
+	"go.opentelemetry.io/collector/pdata/plog"
+)
+
+type logsExporter struct {
+}
+
+func newLogsExporter(
+	ctx context.Context,
+	set exporter.Settings,
+	cfg *Config,
+) (exporter.Logs, error) {
+
+	exp := &logsExporter{}
+
+	return exporterhelper.NewLogs(
+		ctx,
+		set,
+		cfg,
+		exp.consumeLogs,
+		exporterhelper.WithCapabilities(
+			consumer.Capabilities{
+				MutatesData: false,
+			},
+		),
+	)
+}
+
+func (e *logsExporter) consumeLogs(
+	ctx context.Context,
+	logs plog.Logs,
+) error {
+
+	// Commit 4:
+	// Convert plog.Logs into STIX objects.
+
+	return nil
 }
--- exporter/stixexporter/factory.go
+++ exporter/stixexporter/factory.go
@@ -1,11 +1,41 @@
 package stixexporter
 
 import (
+	"context"
+
+	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/exporter"
 )
 
-// NewFactory creates the STIX exporter factory.
+const typeStr = "stix"
+
+// NewFactory creates a new STIX exporter factory.
 func NewFactory() exporter.Factory {
 
-	panic("not implemented")
+	return exporter.NewFactory(
+		typeStr,
+		createDefaultConfig,
+		exporter.WithLogs(
+			createLogsExporter,
+			component.StabilityLevelAlpha,
+		),
+	)
+}
+
+func createDefaultConfig() component.Config {
+
+	return &Config{}
+}
+
+func createLogsExporter(
+	ctx context.Context,
+	set exporter.Settings,
+	cfg component.Config,
+) (exporter.Logs, error) {
+
+	return newLogsExporter(
+		ctx,
+		set,
+		cfg.(*Config),
+	)
 }
